eec45ae86f1d331c9d4aa248a0911518554c7fb1
Lukasz Iwanski <lukasziwanski@outlook.com>
2021-08-18 00:26:55 +0200

    Dodanie metody contact i przykładowych danych oraz dodanie __str__ do obu klas

--- zad7-3.py
+++ zad7-3.py
@@ -3,7 +3,11 @@ class BaseContact:
         self.imie = imie
         self.nazwisko = nazwisko
         self.tel = tel
-
+    def __str__ (self):
+        return f"{self.imie}, {self.nazwisko}, {self.tel}"
+    @property
+    def contact(self):
+        return f"Wybieram numer {self.tel} i dzwonię do {self.imie}"
 class BusinessContact(BaseContact):
     def __init__(self, stanowisko, nazwa_firmy, tel_sluzbowy, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -11,5 +15,16 @@ class BusinessContact(BaseContact):
         self.nazwa_firmy = nazwa_firmy
         self.tel_sluzbowy = tel_sluzbowy
     def __str__ (self):
-        return f"{self.imie} {self.nazwisko} {self.tel} {self.stanowisko} {self.nazwa_firmy} {self.tel_sluzbowy}"
+        return f"{self.imie}, {self.nazwisko}, {self.tel}, {self.stanowisko}, {self.nazwa_firmy}, {self.tel_sluzbowy}"
+    @property
+    def contact(self):
+        return f"Wybieram numer {self.tel_sluzbowy} i dzwonię do {self.imie}"
 
+
+p1=BaseContact(imie="Theresa", nazwisko="McKenzie", tel=4625145216)
+p2=BaseContact(imie="Vickie", nazwisko="Koelpin", tel=18968073059)
+
+buissness_p1=BusinessContact(imie="Theresa", nazwisko="McKenzie", tel=4625145216, stanowisko="Internal Research Analyst",nazwa_firmy="Bahringer Inc", tel_sluzbowy=8454707059)
+buissness_p2=BusinessContact(imie="Vickie", nazwisko="Koelpin", tel=18968073059, stanowisko="Forward Branding Planner",nazwa_firmy="Bauch - Hickle", tel_sluzbowy=8866965686)
+
+print(buissness_p1.contact)
