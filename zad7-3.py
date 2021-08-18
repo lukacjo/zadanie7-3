a96c41b2c4502a0c04c8500a33a3c5089be11a39
Lukasz Iwanski <lukasziwanski@outlook.com>
2021-08-19 00:42:22 +0200

    stworzenie funkcji który tworzy wizytówki z losowych danych

--- zad7-3.py
+++ zad7-3.py
@@ -1,4 +1,6 @@
+
 from faker import Faker
+fake=Faker()
 
 class BaseContact:
     def __init__(self, imie, nazwisko, tel):
@@ -28,10 +30,31 @@ class BusinessContact(BaseContact):
     def label_length(self):           
         return f"Długość imienia i nazwiska razem to {len(self.imie)+len(self.nazwisko)+1} "
 
+
+    
 p1=BaseContact(imie="Theresa", nazwisko="McKenzie", tel="4625145216")
 p2=BaseContact(imie="Vickie", nazwisko="Koelpin", tel="18968073059")
 
 buissness_p1=BusinessContact(imie="Theresa", nazwisko="McKenzie", tel="4625145216", stanowisko="Internal Research Analyst",nazwa_firmy="Bahringer Inc", tel_sluzbowy="8454707059")
 buissness_p2=BusinessContact(imie="Vickie", nazwisko="Koelpin", tel="18968073059", stanowisko="Forward Branding Planner",nazwa_firmy="Bauch - Hickle", tel_sluzbowy="8866965686")
 
+def create_contacts():
+    rodzaj = input("czy wizytówka którą chcesz stowrzyć ma być base czy business?")
+    ilosc = input("ile wizytówek chcesz stworzyć?")
+    if ilosc.isdigit():
 
+        for i in range(int(ilosc)):
+
+            if rodzaj=="base":
+                print (f" imie= {fake.name()}, nazwisko= {fake.last_name()}, tel= {fake.phone_number()}")
+    
+            elif rodzaj=="business":
+                print(f" imie= {fake.name()}, nazwisko= {fake.last_name()}, tel= {fake.phone_number()}, stanowisko={fake.job()}, nazwa firmy={fake.company()}, tel_sluzbowy={fake.phone_number()}")
+            else:
+                print("należy wpisać base lub business")
+                exit(1)
+    else:
+        print("należy podać liczbę")
+        exit(1)
+        
+create_contacts()
